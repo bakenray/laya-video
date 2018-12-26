25179f65b8a4e02e3d68542a67b34278716a8f72
bakenray <lpqstrong@163.com>
2018-12-26 11:10:19 +0800

    hello world

---
+++ src/1-helloworld.js
@@ -0,0 +1,18 @@
+
+(function(){
+    Laya.init(600,400,Laya.WebGL)
+    Laya.stage.scaleMode = Laya.Stage.SCALE_NOSCALE
+    Laya.stage.alignH = Laya.Stage.ALIGN_CENTER
+    Laya.stage.alignV = Laya.Stage.ALIGN_MIDDLE
+    Laya.stage.screenMode = Laya.Stage.SCREEN__NONE
+
+    var tx =new Laya.Text()
+    tx.text = 'hello world'
+    tx.pos(Laya.stage.width/2,Laya.stage.height/2)
+    tx.color = '#ffffff'
+    tx.fontSize = 55
+    Laya.stage.addChild(tx)
+    tx.pivot(tx.width/2,tx.height/2)
+})()
+
+
